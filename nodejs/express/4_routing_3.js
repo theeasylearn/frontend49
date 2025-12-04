55dcae3a797ecac87dd5353d366625041fa7a6b9
theeasylearn <theeasylearn@gmail.com>
2025-12-04 20:09:01 +0530

    routing

--- nodejs/express/4_routing_3.js
+++ nodejs/express/4_routing_3.js
@@ -52,33 +52,59 @@ app.post(ROUTE_NAME, function (request, response) {
 //http://localhost:5000/contact
 app.put(ROUTE_NAME, function (request, response) {
     let { id, name, email, mobile, message } = request.body;
-
+    console.log(id, name, email, mobile);
     let output = null;
     if (id === undefined || name === undefined || email === undefined || mobile === undefined || message === undefined) {
         output = [{ 'error': 'input missing' }];
     }
     else {
-        console.log(id, name, email, mobile);
-        let temp = customers.filter(function (item) {
-            if (item.id === parseInt(id))
-                return item;
+
+        let isUpdate = false;
+        customers.forEach(function (item, index) {
+            if (item.id === parseInt(id)) {
+                customers[index].name = name;
+                customers[index].email = email;
+                customers[index].mobile = mobile;
+                customers[index].message = message;
+                isUpdate = true;
+            }
         });
-        if (temp.length === 0) {
+        if (isUpdate === false) {
             output = [{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'contact not found' }];
         }
         else {
-            output = [{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'contact found' }];
+            output = [{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'contact updated' }];
         }
-        response.json(output);
     }
+    response.json(output);
 });
 
 //create delete route
 //http://localhost:5000/contact
 app.delete(ROUTE_NAME, function (request, response) {
-    response.send("delete request received for contact route");
+    let id = request.body.id;
+    if (id === undefined) {
+        output = [{ 'error': 'input missing' }];
+    }
+    else {
+        let isDeleted = false;
+        customers = customers.filter(function (item) {
+            if (item.id !== parseInt(id)) {
+                return item
+            }
+            else 
+            {
+                isDeleted = true;
+            }
+        });
+        if (isDeleted === false) {
+            output = [{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'contact not found' }];
+        }
+        else {
+            output = [{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'contact deleted successfully' }];
+        }
+    }
+    response.json(output);
 });
-
-
 app.listen(5000);
 console.log('ready to accept request');
