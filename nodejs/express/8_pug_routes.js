b7f20da9c67ad39e640f8d72406a61165bf15182
theeasylearn <theeasylearn@gmail.com>
2026-01-17 19:33:37 +0530

    pug

---
+++ nodejs/express/8_pug_routes.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+app.set('view engine','pug')
+app.set('views','views')
+//define route
+app.get("/one",function(request,response){
+    response.render('one'); //it will execute pug file and nodejs returns html page 
+});
+
+app.get("/two",function(request,response){
+    response.render('two'); //it will execute pug file and nodejs returns html page 
+});
+
+app.get("/three",function(request,response){
+    response.render('three'); //it will execute pug file and nodejs returns html page 
+});
+
+app.listen(5000);
+console.log('ready...');
