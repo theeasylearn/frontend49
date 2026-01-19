28f1df7a785c1c3f6e6a799ffbe307dc48e7f67d
theeasylearn <theeasylearn@gmail.com>
2026-01-19 19:36:43 +0530

    pug

--- nodejs/express/8_pug_routes.js
+++ nodejs/express/8_pug_routes.js
@@ -1,18 +1,28 @@
 var express = require('express')
 var app = express()
-app.set('view engine','pug')
-app.set('views','views')
+app.set('view engine', 'pug')
+app.set('views', 'views')
 //define route
-app.get("/one",function(request,response){
+app.get("/one", function (request, response) {
     response.render('one'); //it will execute pug file and nodejs returns html page 
 });
 
-app.get("/two",function(request,response){
+app.get("/two", function (request, response) {
     response.render('two'); //it will execute pug file and nodejs returns html page 
 });
 
-app.get("/three",function(request,response){
+app.get("/three", function (request, response) {
     response.render('three'); //it will execute pug file and nodejs returns html page 
+});
+
+app.get("/variable", function (request, response) {
+    response.render("variables", {
+        name: "Ram kumar",
+        gender: "Male",
+        age: 30,
+        weight: 65.25,
+        mobile: "1234567890"
+    });
 });
 
 app.listen(5000);
