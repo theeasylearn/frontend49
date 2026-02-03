4b72afba604c40dfd8f06f7fdee1155cf4078a79
theeasylearn <theeasylearn@gmail.com>
2026-02-03 20:00:47 +0530

    storage

--- nodejs/mongodb/storage.js
+++ nodejs/mongodb/storage.js
@@ -1,6 +1,14 @@
+var { dbPromise } = require('./db_connection');
 function insert(request, response) {
-   var document = request.body;
-   
+    var document = request.body;
+    //document insertion code
+    dbPromise.then((database) => {
+        database.collection('company').insertOne(document);
+        response.json([{ 'error': 'no' }, { 'message': 'document inserted successfully' }])
+    }).catch((error) => {
+        console.log('error = ' + error);
+        response.json([{ 'error': 'oops something went wrong,please try after sometime' }])
+    });
 }
 function update(request, response) {
     console.log('update document');
@@ -9,7 +17,18 @@ function deleteDocument(request, response) {
     console.log('delete document');
 }
 function select(request, response) {
-    console.log('select document');
+    dbPromise.then((database) => {
+        //fetch all documents 
+        database.collection('data').find({}).toArray((error, documents) => {
+            if (error)
+                console.log(error);
+            else
+                console.log(documents);
+        })
+    }).catch((error) => {
+        console.log('error = ' + error);
+    });
+
 }
 
 module.exports.insert = insert;
