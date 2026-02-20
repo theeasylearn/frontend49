0c2b97afe0c6c7f6c129ca8a4ac881aa23c56424
theeasylearn <theeasylearn@gmail.com>
2026-02-20 20:02:06 +0530

    reactjs

---
+++ reactjs/app1/src/1_basic.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; //here ./ is used because index.css file is in src folder
+//create virtual DOM 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div>
+<h1>Welcome, to world of reactjs.....</h1>
+</div> );
--- reactjs/app1/src/2_variable_and_expression.js
+++ reactjs/app1/src/2_variable_and_expression.js
@@ -7,14 +7,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 var num1 = 100;
 var num2 = 9;
 var output = (<div>
-    <h1>how to apply css class</h1>
+    <h1>Expression in react</h1>
     <hr/>
     <b>num1 = {num1} num2 = {num2}</b>
-    <ol className='maths'>
-        <li className='maths-item'>Addition = {num1 + num2}</li>
-        <li className='maths-item'>Subtraction {num1 - num2}</li>
-        <li className='maths-item'>Multiplication {num1 * num2}</li>
-        <li className='maths-item'>Division {num1 / num2}</li>
+    <ol>
+        <li>Addition = {num1 + num2}</li>
+        <li>Subtraction {num1 - num2}</li>
+        <li>Multiplication {num1 * num2}</li>
+        <li>Division {num1 / num2}</li>
     </ol>
 </div>)
 root.render(output)
