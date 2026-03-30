c8878fdf5cc8ef94e73ecf54d40e864f530488a5
theeasylearn <theeasylearn@gmail.com>
2026-03-30 19:11:22 +0530

    routing

---
+++ reactjs/app2/src/2_routing.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Contact from './contactus';
+import Courses from './courses';
+import AboutUs from './about';
+import Products from './products';
+import Services from './services';
+import Testimonials from './testimonials'
+import Gallery from './gallery'
+import Home from './home'
+import PageNotFound from './404';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function MyRoute()
+{
+    return (<BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} index />
+            <Route path='/contactus' element={<Contact />} />
+            <Route path='/courses' element={<Courses />} />
+            <Route path='/aboutus' element={<AboutUs />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/testimonials' element={<Testimonials />} />
+            <Route path='/gallery' element={<Gallery />} />
+            {/* create route for page not found */}
+            <Route path='*' element={<PageNotFound />} />
+        </Routes>
+    </BrowserRouter>)
+}
+root.render(<MyRoute />);
--- reactjs/app2/src/index.js
+++ reactjs/app2/src/index.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Contact from './contactus';
-import Courses from './courses';
-import AboutUs from './about';
-import Products from './products';
-import Services from './services';
-import Testimonials from './testimonials'
-import Gallery from './gallery'
-import Home from './home'
-import PageNotFound from './404';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-function MyRoute()
-{
-    return (<BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />} index />
-            <Route path='/contactus' element={<Contact />} />
-            <Route path='/courses' element={<Courses />} />
-            <Route path='/aboutus' element={<AboutUs />} />
-            <Route path='/products' element={<Products />} />
-            <Route path='/services' element={<Services />} />
-            <Route path='/testimonials' element={<Testimonials />} />
-            <Route path='/gallery' element={<Gallery />} />
-            {/* create route for page not found */}
-            <Route path='*' element={<PageNotFound />} />
-        </Routes>
-    </BrowserRouter>)
+
+function Post(props) {
+    return (<tr>
+        <td>{props.postid}</td>
+        <td>{props.userid}</td>
+        <td>{props.title}</td>
+        <td>{props.detail}</td>
+    </tr>);
 }
-root.render(<MyRoute />);
+function DisplayPosts() {
+    //create state array
+    let [posts, setPosts] = useState([]);
+    //use useEffect Hook
+    useEffect(() => {
+        let apiAddress = "https://jsonplaceholder.typicode.com/posts";
+        fetch(apiAddress)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+            })
+            .catch((error) => console.log(error));
+    });
+    return (<div className="container mt-5">
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="card shadow">
+                    <div className="card-header text-bg-primary text-white">
+                        <h4 className="mb-0">Posts Data</h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="table-responsive">
+                            <table className="table table-bordered table-striped table-hover">
+                                <thead className="table-dark">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>User ID</th>
+                                        <th>Title</th>
+                                        <th>Body</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                   {posts.map((item) => {
+                                        return <Post postid={item.id} userid={item.userId}
+                                        title={item.title} detail={item.body} />
+                                   })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<DisplayPosts />);
